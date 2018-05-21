aaa29925b85dd9d8be9ec711e5aec5483e233609
Joppe Aarts <joppe@apestaartje.info>
2018-05-21 15:49:46 +0200

    Added rand/random functions and modified format function

--- src/format.ts
+++ src/format.ts
@@ -1,56 +1,36 @@
-import * as string from 'string/reverse';
-
-const THREE_DIGITS:RegExp = /(\d{3})/g;
-
-/**
- * @interface FormatFunctionInterface
- */
-export interface FormatFunctionInterface {
-    /**
-     * @param {string} input
-     * @param {number} decimalCount
-     * @param {string} decimalPoint
-     * @param {string} thousandsSeperator
-     * @returns {string}
-     */
-    (input:number, decimalCount?:number, decimalPoint?:string, thousandsSeperator?:string):string;
-}
-
 /**
  * Format a number with decimal separator and thousands grouping separator
- *
- * @param {string} input
- * @param {number} [decimalCount=2]
- * @param {string} [decimalPoint='.']
- * @param {string} [thousandsSeperator=',']
- * @returns {string}
  */
-export const format:FormatFunctionInterface = (input:number, decimalCount:number = 2, decimalPoint:string = '.', thousandsSeperator:string = ','):string => {
+
+export type FormatFunction = (input: number, decimalCount?: number, decimalPoint?: string, thousandsSeperator?: string) => string;
+
+// tslint:disable-next-line max-line-length
+export const format: FormatFunction = (input: number, decimalCount: number = 2, decimalPoint: string = '.', thousandsSeperator: string = ','): string => {
     // Cast the number to a string with the desired decimals
-    const str:string = input.toFixed(decimalCount);
+    const str: string = input.toFixed(decimalCount);
 
     // Split the number in a integer and a fraction part
-    const [integer, fraction]:string[] = str.split('.');
-    let output:string = '';
+    const [integer, fraction]: Array<string> = str.split('.');
+    const output: Array<string> = [];
+    const length: number = integer.length;
+    const rest: number = length % 3;
 
-    // If there are more then 3 integers the thousands separator is relevant
-    if (integer.length > 3) {
-        // Reverse the order of the integers, then we can place a thousandsSeperator after every 3 characters
-        const reversedInteger:string = string.reverse(integer);
-        const reversedSeparatedInteger:string = reversedInteger.replace(THREE_DIGITS, (match:string):string => {
-            return `${match}${thousandsSeperator}`;
-        });
+    if (length > 3) {
+        output.push(integer.substr(0, rest));
 
-        // Reverse the integers back again
-        output += string.reverse(reversedSeparatedInteger);
+        for (let i: number = rest; i < length; i += 3) {
+            output.push(thousandsSeperator);
+            output.push(integer.substr(i, 3));
+        }
     } else {
-        output += integer;
+        output.push(integer);
     }
 
     // Show a decimal char if needed
-    if (0 < decimalCount) {
-        output += `${decimalPoint}${fraction}`;
+    if (decimalCount > 0) {
+        output.push(decimalPoint);
+        output.push(fraction);
     }
 
-    return output;
+    return output.join('');
 };
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * This file exports all functions from this lib
+ */
+
+ export * from './format';
+ export * from './rand';
+ export * from './random';
