1c804b8606359a592e51e45e4350f6182eca84f7
Joppe Aarts <joppe@apestaartje.info>
2020-03-24 17:05:22 +0100

    fixed jest handling getRandomValues

--- src/rand.ts
+++ src/rand.ts
@@ -3,6 +3,8 @@
  * 0 <= value <= 1
  */
 
+const MAX_VALUE: number = Math.pow(2, 32) - 1;
+
 export function rand(): number {
     const byteArray: Uint32Array = new Uint32Array(1);
 
@@ -10,7 +12,6 @@ export function rand(): number {
 
     // 0 <= value <= max
     const value: number =  byteArray[0];
-    const max: number = Math.pow(2, 32) - 1;
 
-    return value / max;
+    return value / MAX_VALUE;
 }
